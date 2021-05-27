5118316c118601e76b9bb2338e044be270e63076
alice(ShilkinaMI) <alice@example.com>
2021-05-27 20:01:15 +0300

    деление

--- lab/main.cpp
+++ lab/main.cpp
@@ -8,6 +8,7 @@ cout << "Enter A and B: ";
 int a, b;
 cout << "A + B = " << a + b << '\n'
  << "A - B = " << a - b << '\n'
-  << "A * B = " << a * b << '\n';
+  << "A * B = " << a * b << '\n'
+  << "A / B = " << a / b << '\n';
 cin >> a >> b;
 }
