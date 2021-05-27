8d6a92449ffe98e9a9af391753b421dbc6eefd8b
bob(ShilkinaMI) <bob@example.com>
2021-05-27 20:06:09 +0300

    min

--- lab/main.cpp
+++ lab/main.cpp
@@ -5,10 +5,16 @@ using namespace std;
 int main()
 {
 cout << "Enter A and B: ";
-int a, b,max;
+HEAD
+int a, b,max,min;
+
+
+
 cout << "A + B = " << a + b << '\n'
  << "A - B = " << a - b << '\n'
   << "A * B = " << a * b << '\n'
   << "A / B = " << a / b << '\n';
-cin >> a >> b>>max;
-}
+
+cin >> a >> b>>max,min;
+return 0; }
+
